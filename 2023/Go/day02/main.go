0856d413fdcd644534da7526d1a232f026ea00b6
--gloval <--global>
2024-05-10 21:07:45 -0500

    Template

---
+++ 2023/Go/day02/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Printf("Part1: %d\n", part1())
+	fmt.Printf("Part2: %d\n", part2())
+}
---
+++ 2023/Go/day02/part2.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func part2() int{
+	file, err := os.Open("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+	}
+
+	return 0
+}
